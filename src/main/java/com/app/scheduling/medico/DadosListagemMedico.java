914f58680d5ef27c1852908aed92b1c4e1153144
Jessica Beatriz Monteiro <jessicabeatrizmonteiro1994@gmail.com>
2024-11-10 00:53:15 -0300

    ajuste diretorios

--- src/main/java/com/app/scheduling/medico/DadosListagemMedico.java
+++
@@ -1,7 +0,0 @@
-package com.app.scheduling.medico;
-
-public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
-    public DadosListagemMedico(Medico medico){
-        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
-    }
-}
